4a8e45a21ce47fa65bbb75231b1375c3f33ee6d1
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2022-10-19 17:29:19 +0000

    Deploying to gh-pages from @ RuslanManoilo/IceTeam@dc4dd6f12bad88c2ff9e9011cbd720b666ec2afe 🚀

--- index.5db09202.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".owl-carousel").owlCarousel({items:1,loop:!0,dots:!0,margin:200,autoplay:!0,autoplayTimeout:2500,autoplayHoverPause:!0,smartSpeed:1750,dragEndSpeed:500,dotsSpeed:750})}));
-//# sourceMappingURL=index.5db09202.js.map
